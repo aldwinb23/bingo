14a9ff9b18167721bbf71b03fb879a2ccb0a7d10
aldwinb <aldwinb23@gmail.com>
2023-02-03 23:00:36 -0800

    install react icon

--- client/src/Navbar/Navbar.js
+++ client/src/Navbar/Navbar.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import './navbar.css'
+import { AiOutlineBars } from 'react-icons/ai';
 
 function Navbar() {
   return (
-    <div className='navbar__main'>BINGO</div>
+    <div className='navbar__main'>
+      <div>
+        <AiOutlineBars />
+      </div>
+      <div>
+        BINGO
+      </div>
+      <div className='navbar__name'>
+        Name
+      </div>
+    </div>
   )
 }
 
