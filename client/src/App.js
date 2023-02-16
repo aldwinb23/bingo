709f224d9bc929b584ce436e40e6fa841d19e252
aldwinb <aldwinb23@gmail.com>
2023-02-15 20:40:04 -0800

    add random number generator for all letters

--- client/src/App.js
+++ client/src/App.js
@@ -4,39 +4,103 @@ import Navbar from './Navbar/Navbar';
 
 function App() {
 
-  function getRandomInt(max) {
-    return Math.floor(Math.random() * max) + 1;
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
   }
-
-  let bingo_b1 = Math.floor(Math.random() * 14) + 1;
-  let bingo_b2 = Math.floor(Math.random() * 14) + 1;
-  let bingo_b3 = Math.floor(Math.random() * 14) + 1;
-  let bingo_b4 = Math.floor(Math.random() * 14) + 1;
-  let bingo_b5 = Math.floor(Math.random() * 14) + 1;
 
   let bingo_b = [];
   while (bingo_b.length < 5) {
-    let newNum = getRandomInt(15)
-    bingo_b.push(newNum);
+    let newNum = getRandomInt(1, 15)
+    if (!bingo_b.includes(newNum)) {
+      bingo_b.push(newNum);
+    }
   }
+  
+  let bingo_i = [];
+  while (bingo_i.length < 5) {
+    let newNum = getRandomInt(16, 30)
+    if (!bingo_i.includes(newNum)) {
+      bingo_i.push(newNum);
+    }
+  }
+  let bingo_n = [];
+  while (bingo_n.length < 5) {
+    let newNum = getRandomInt(31, 45)
+    if (!bingo_n.includes(newNum)) {
+      bingo_n.push(newNum);
+    }
+  }
+  let bingo_g = [];
+  while (bingo_g.length < 5) {
+    let newNum = getRandomInt(46, 60)
+    if (!bingo_g.includes(newNum)) {
+      bingo_g.push(newNum);
+    }
+  }
+  let bingo_o = [];
+  while (bingo_o.length < 5) {
+    let newNum = getRandomInt(61, 75)
+    if (!bingo_o.includes(newNum)) {
+      bingo_o.push(newNum);
+    }
+  }
+
+
 
   return (
     <div className="App">
       <Navbar />
       <header className="App-header">
         <div>
-          {bingo_b1} - 
-          {bingo_b2} - 
-          {bingo_b3} - 
-          {bingo_b4} - 
-          {bingo_b5} - 
           Log in as:
         </div>
+        <div>
+        B -
         {bingo_b[0]} - 
         {bingo_b[1]} - 
         {bingo_b[2]} - 
         {bingo_b[3]} - 
         {bingo_b[4]}
+        </div>
+
+        <div>
+        I -
+        {bingo_i[0]} - 
+        {bingo_i[1]} - 
+        {bingo_i[2]} - 
+        {bingo_i[3]} - 
+        {bingo_i[4]}
+        </div>
+
+        <div>
+        N -
+        {bingo_n[0]} - 
+        {bingo_n[1]} - 
+        {bingo_n[2]} - 
+        {bingo_n[3]} - 
+        {bingo_n[4]}
+        </div>
+
+        <div>
+        G -
+        {bingo_g[0]} - 
+        {bingo_g[1]} - 
+        {bingo_g[2]} - 
+        {bingo_g[3]} - 
+        {bingo_g[4]}
+        </div>
+
+        <div>
+        O -
+        {bingo_o[0]} - 
+        {bingo_o[1]} - 
+        {bingo_o[2]} - 
+        {bingo_o[3]} - 
+        {bingo_o[4]}
+        </div>
+
         <div>
           <button>
             Player
